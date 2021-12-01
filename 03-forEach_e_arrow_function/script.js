63034d33ba31617b8699e7c3fd6c5e7fe2fb15ad
Gabriel <bielmarques922@gmail.com>
2021-11-30 22:36:32 -0300

    forEach e Arrow Function - Parte 2

--- 03-forEach_e_arrow_function/script.js
+++ 03-forEach_e_arrow_function/script.js
@@ -72,20 +72,25 @@ imgs4.forEach(item => console.log(item));
 
 // EXERCÍCIO
 // Mostre no console cada parágrado do site
+const paragrafos = document.querySelectorAll('p');
+console.log(paragrafos)
 
+paragrafos.forEach((item) => {
+  console.log(item.innerText);
+})
 // Mostre o texto dos parágrafos no console
 
 // Como corrigir os erros abaixo:
 const imgs5 = document.querySelectorAll('img');
 
-imgs5.forEach(item, index => {
+imgs5.forEach((item, index) => {
   console.log(item, index);
 });
 
-//let i = 0;
-// imgs.forEach( => {
-//   console.log(i++);
-// });
+let i1 = 0;
+imgs.forEach(() => {
+ console.log(i1++);
+ });
 
 imgs.forEach(() => i++);
 
