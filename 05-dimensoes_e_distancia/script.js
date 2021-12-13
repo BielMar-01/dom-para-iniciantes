1fcc6fc89b709d955c19f7e054efb2f4f1812d0a
Gabriel <bielmarques922@gmail.com>
2021-12-13 10:47:24 -0300

    Dimensões e Distâncias - Parte 2

--- 05-dimensoes_e_distancia/script.js
+++ 05-dimensoes_e_distancia/script.js
@@ -60,14 +60,44 @@ if(small.matches) {
 
 
 // EXERCÍCIO
-// Verifique a distância da primeira imagem
-// em relação ao topo da página
+// Verifique a distância da primeira imagem em relação ao topo da página
+const img = document.querySelector('img');
+const imgTop = img.offsetTop;
+
+console.log(imgTop);
 
 // Retorne a soma da largura de todas as imagens
+const imagens = document.querySelectorAll('img');
 
-// Verifique se os links da página possuem
-// o mínimo recomendado para telas utilizadas
-// com o dedo. (48px/48px de acordo com o google)
+function somaImagens() {
+  let soma = 0;
+  imagens.forEach((imagem) => {
+    soma = soma + imagem.offsetWidth;
+  });
+  console.log(soma);
+}
 
-// Se o browser for menor que 720px,
-// adicione a classe menu-mobile ao menu
+window.onload = function() {
+  somaImagens();
+}
+
+// Verifique se os links da página possuem o mínimo recomendado para telas utilizadas com o dedo. (48px/48px de acordo com o google)
+const links = document.querySelectorAll('a');
+
+links.forEach((link) => {
+  const linkWidth = link.offsetWidth;
+  const linkHeight = link.offsetHeight;
+  if(linkWidth >= 48 && linkHeight >= 48) {
+    console.log(link, 'Possui boa acessibilidade')
+  } else {
+    console.log(link, 'Não possui boa acessibilidade')
+  }
+});
+
+// Se o browser for menor que 720px, adicione a classe menu-mobile ao menu
+const browserSmall = window.matchMedia('(max-width: 720px)').matches;
+
+if (browserSmall) {
+  const menu = document.querySelector('.menu');
+  menu.classList.add('menu-mobile');
+}
